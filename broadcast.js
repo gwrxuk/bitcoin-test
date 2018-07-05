879d0cbe572e217f6e8e42f2cd7d0667f98b933b
Emile Liu(劉榮樺) <Emile_Liu@HTC.COM>
2018-07-05 16:09:41 +0800

    transaction

--- broadcast.js
+++ broadcast.js
@@ -1,2 +1,6 @@
 const https = require("https");
-var postData = JSON.
+var postData = JSON.stringify({tx:"4141691e552a4ce9e86b9bcd0a3a35754552e01f093bce85988168cee06e2fbe"});
+var options = {
+
+
+};
---
+++ transaction.js
@@ -0,0 +1,8 @@
+const bitcoin = require("bitcoinjs-lib");
+
+function rng(){
+    return Buffer.from('YT8dAtK4d16A3P1z+TpwB2jJ4aFH3g9M1EioIBkLEV4=', 'base64');
+}
+
+const alice = bitcoin.ECPair.fromWIF('L1uyy5qTuGrVXrmrsvHWHgVzW9kKdrp27wBC7Vs6nZDTF2BRUVwy');
+const txb = new bitcoin.TransactionBuilder();
